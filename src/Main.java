cb29f5daea5a2dd321179e961da9f3d33bdfc970
ElizaMira <elizabeth.hmu@gmail.com>
2023-06-01 20:37:52 -0500

    Assignment-Lecture3b

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,39 @@
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Customer c1=new Customer("Sam","Smith","S11111");
+        Customer c2=new Customer("Joe","Doe","S22222");
+        Customer c3=new Customer("Eric","DD","S33333");
+        Address A1=new Address("North Street","Chicago","Iowa","52557");
+        Address A2=new Address("South Street","Fairfield","Iowa","52557");
+        Address A3=new Address("East Street","Chicago","Illinois","12345");
+        c1.setBillingAddress(A1);
+        c1.setShippingAddress(A1);
+        c2.setBillingAddress(A2);
+        c2.setShippingAddress(A2);
+        c3.setBillingAddress(A3);
+        c3.setShippingAddress(A3);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Customer [] CustomerList=new Customer[3];
+        CustomerList[0]=c1 ;
+        CustomerList[1]=c2 ;
+        CustomerList[2]=c3 ;
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        for (int i=0;i<3;i++)
+        {
+            if (CustomerList[i].getBillingAddress().getCity() =="Chicago")
+            {
+                System.out.println(CustomerList[i]);
+            }
         }
+
+
+
+
+
+
     }
-}
+    }
