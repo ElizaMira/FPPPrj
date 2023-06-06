7aebac009f67d77b39b5825e84530ffdac95beb9
ElizaMira <135084185+ElizaMira@users.noreply.github.com>
2023-06-05 23:05:55 -0500

    Assignment-Lecture5
    Problem 1

---
+++ src/Circle.java
@@ -0,0 +1,20 @@
+package Inheritance;
+
+public class Circle extends Shape{
+    double radius;
+    Circle(String color,double radius)
+    {
+        super(color);
+        this.radius=radius;
+    }
+    @Override
+    public double calculateArea()
+    {
+        return(Math.PI*radius*radius);
+    }
+    @Override
+    public double calculatePerimeter()
+    {
+        return(2*Math.PI*radius);
+    }
+}
