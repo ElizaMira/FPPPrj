1d5648881a167ac7cfe2f7d0f5c0bf381c8a74d4
ElizaMira <135084185+ElizaMira@users.noreply.github.com>
2023-06-06 23:04:19 -0500

    Assignment5-Problem4

---
+++ src/Assignment-Lecture5/Employee.java
@@ -0,0 +1,29 @@
+package Assignment5_Problem4;
+
+public abstract class Employee {
+    private String firstName;
+    private String lastName;
+    private String socialSecurityNumber;
+
+    Employee(String firstName,String lastName,String socialSecurityNumber)
+    {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.socialSecurityNumber=socialSecurityNumber;
+    }
+    public abstract double  getPayment();
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    public String getLastName()
+    {
+        return lastName;
+    }
+    public String getSocialSecurityNumber()
+    {
+        return socialSecurityNumber;
+    }
+
+}
