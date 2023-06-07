34cfe31a6371ff5e1394dfbc7c4efac6fb284aa4
ElizaMira <135084185+ElizaMira@users.noreply.github.com>
2023-06-06 22:38:06 -0500

    Add files via upload
    Problem 3

---
+++ src/Assignment-Lecture5/FaceMaker.java
@@ -0,0 +1,9 @@
+package Assignment5;
+
+public class FaceMaker implements Figure{
+
+    public void getFigure() {
+        System.out.print(":)");
+    }
+
+}
---
+++ src/Assignment-Lecture5/UpwardHat.java
@@ -0,0 +1,11 @@
+package Assignment5;
+
+public class UpwardHat implements Figure{
+
+
+    public  void getFigure()
+    {
+        System.out.print("^");
+    }
+
+    }
