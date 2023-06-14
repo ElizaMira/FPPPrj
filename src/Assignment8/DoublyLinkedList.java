3c62709f7438c5b9357519d5614941fc3d78fb46
ElizaMira <135084185+ElizaMira@users.noreply.github.com>
2023-06-13 20:49:22 -0500

    Prob4

---
+++ src/Assignment8/DoublyLinkedList.java
@@ -0,0 +1,94 @@
+package Assignment8;
+
+import DataStructure.MyDoublyLinkedList;
+
+import javax.management.NotificationEmitter;
+
+public class DoublyLinkedList {
+	
+		Node header;
+		DoublyLinkedList(){
+			header = new Node();
+
+		}
+		//adds to the end of the list
+		public void addLast(String item){
+
+
+				Node currentNode = header;
+				while (currentNode.next != null) {
+					currentNode = currentNode.next;
+				}
+
+				//last node is current node
+
+				Node node = new Node();
+				node.value = item;
+				node.previous = currentNode;
+				currentNode.next = node;
+
+			}
+
+
+
+	public boolean remove(String item){
+		Node prevNode = header;
+		Node currentNode = header.next;
+
+		while (currentNode.next!=null && currentNode.value!=null){
+			if (currentNode.value.equals(item)){
+				prevNode.next = currentNode.next;
+				currentNode = currentNode.next;
+				currentNode.previous = prevNode;
+				return true;
+			}else {
+				prevNode = currentNode;
+				currentNode = currentNode.next;
+			}
+		}
+		return false;
+	}
+
+	@Override
+		public String toString() {
+			
+			StringBuilder sb = new StringBuilder();
+			toString(sb, header);
+			return sb.toString();
+			
+		}
+		private void toString(StringBuilder sb, Node n) {
+			if(n==null) return;
+			if(n.value != null) sb.append(" " + n.value);
+			toString(sb, n.next);
+		}
+
+
+		
+		class Node {
+			String value;
+			Node next;
+			Node previous;
+
+//			Node(String value)
+//			{
+//				this.value=value;
+//			}
+			
+			public String toString() {
+				return value == null ? "null" : value;
+			}
+		}
+	
+		public static void main(String[] args){
+			DoublyLinkedList list = new DoublyLinkedList();
+			list.addLast("Bob");
+			list.addLast("Harry");
+			list.addLast("Steve");
+			list.addLast("Carol");
+
+			System.out.println(list);
+			list.remove("Harry");
+			System.out.println(list);
+		}
+}
